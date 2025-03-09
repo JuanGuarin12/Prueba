1c344e9cd4fdd0c80f9fa4079e5c97e49d176532
emmanuellsk8 <emmanuel50354@gmail.com>
2025-03-08 21:53:42 -0500

    se añaden assets e index2

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+function toggle (){
+
+const moto = document.getElementById('moto');
+
+if (moto.style.display === 'none'){
+    moto.style.display = 'flex';
+}else{
+    moto.style.display = 'none';
+}
+
+} 
